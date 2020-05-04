5c997d95c13a30da85975d79c69b6d407b9b9d00
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-04 12:43:37 +0900

    一部の言葉で検索できる

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange', 'clementine', 'mandarine', 'tangerine', 'papaya', 'passionfruit', 'peach', 'pear', 'persimmon', 'physalis', 'plum/prune', 'pineapple', 'pomegranate', 'raspberry', 'rambutan', 'star fruit', 'strawberry'];
 
 $(function () {
+
   let list = $("#list");
   function appendList(world) {
-    let item = $('<li class="list>').append(world);
+    let item = $('<li class="list">').append(world);
     list.append(item);
   }
   $("#submit").on("click", function () {
@@ -17,7 +18,8 @@ $(function () {
       };
     });
 
-    if ($(".list").lengt === 0);
-    $("#result").text("一致するものはありませんでした");
+    if ($(".list").length === 0) {
+      appendList("一致する果物がありませんでした");
+    }
   });
 });
