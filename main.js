f867e0d09ee902b4d40964255b26faf23cf2a33a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-04 12:08:04 +0900

    入力された値と配列の要素を比較して一致したものを見つける

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange', 'clementine', 'mandarine', 'tangerine', 'papaya', 'passionfruit', 'peach', 'pear', 'persimmon', 'physalis', 'plum/prune', 'pineapple', 'pomegranate', 'raspberry', 'rambutan', 'star fruit', 'strawberry'];
+
+$(function () {
+  $("#submit").on("click", function () {
+    let input = $("#keyword").val();
+    $.each(fruits, function (i, fruit) {
+      if (input === fruit) {
+        $("#result").text(input);
+        return false;
+      } else {
+        $("#result").text("一致する果物はありませんでした。");
+      }
+    });
+  });
+});
